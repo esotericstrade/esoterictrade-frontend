cb6ea6d7c2777d5396cf9f875743d0d346e9869b
Girish Sawant <girishsawant999.gs@gmail.com>
2025-05-21 14:39:33 +0530

    feat: integrate clsx for conditional class management in Sidebar component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -10,10 +10,12 @@ import {
   Lightning,
   SignOut,
   Smiley,
+  UserCircle,
   Users,
   WaveTriangle,
 } from "@phosphor-icons/react";
-import { Avatar, Divider, Menu } from "antd";
+import { Divider, Menu } from "antd";
+import clsx from "clsx";
 import { useNavigate } from "react-router-dom";
 
 const MENU_ITEMS = [
@@ -76,41 +78,91 @@ const Sidebar = () => {
         }))}
       />
       <Divider className="my-3" />
-      <Menu
-        selectedKeys={[]}
-        items={[
-          {
-            key: "community",
-            icon: <Smiley size={16} weight="duotone" />,
-            label: "Community",
-            onClick: () => navigate("/community"),
-          },
-          {
-            key: "settings",
-            icon: <Gear size={16} weight="duotone" />,
-            label: "Settings",
-            onClick: () => navigate("/settings"),
-          },
-          {
-            key: "help",
-            icon: <Headset size={16} weight="duotone" />,
-            label: "Help & support",
-            onClick: () => navigate("/help-and-support"),
-          },
-          {
-            key: "logout",
-            icon: (
-              <SignOut size={16} weight="duotone" className="!fill-red-500" />
-            ),
-            label: "Logout",
-            onClick: onLogout,
-            className:
-              "!text-red-600 hover:!bg-red-100 [&.ant-menu-item-selected]:!bg-red-600",
-          },
-        ]}
-      />
 
-      <div className="grid grid-cols-[auto_1fr_auto] px-1.5 py-1.5 items-center gap-1.5 overflow-hidden mt-auto w-full bg-gray-100 rounded-lg border border-gray-200">
+      <div className="mt-auto">
+        <hr className="mb-3 border-gray-200" />
+
+        <div className={clsx("w-full")}>
+          <div
+            role="button"
+            className={clsx(
+              "group grid grid-rows-[1fr] items-center rounded-lg bg-white py-1.5 shadow-[0px_0px_0px_1px_#dadadd] transition-[grid-template-rows,max-height,opacity] duration-300 ease-in-2 hover:bg-gray-50",
+              "group-hover:grid-rows-[1fr_auto]"
+            )}
+          >
+            <div className="flex items-center px-2">
+              <UserCircle
+                weight="fill"
+                className="mx-auto text-blue-600"
+                size={18}
+              />
+              <div
+                className={clsx(
+                  "ms-2.5 flex flex-1 items-center gap-1.5 overflow-hidden transition-all duration-300 ease-in-2",
+                  "max-w-[160px] scale-x-100 opacity-100"
+                )}
+                style={{ transformOrigin: "left" }}
+              >
+                <p className="flex-1 truncate text-left text-[13px] font-medium text-[#42424a]">
+                  {user?.first_name} {user?.last_name}
+                </p>
+                <CaretDown
+                  weight="fill"
+                  size={16}
+                  className="text-gray-500 group-hover:rotate-180 transition-transform duration-300 ease-in-2"
+                />
+              </div>
+            </div>
+            <div
+              className={clsx(
+                "grid max-h-0 gap-2 overflow-hidden opacity-0 transition-[max-height,opacity] duration-300 ease-in-2",
+                "group-hover:max-h-40 opacity-100"
+              )}
+            >
+              <hr className="mx-2 mt-2 border-gray-200" />
+              <Menu
+                selectedKeys={[]}
+                items={[
+                  {
+                    key: "logout",
+                    icon: (
+                      <SignOut
+                        size={16}
+                        weight="duotone"
+                        className="!fill-red-500"
+                      />
+                    ),
+                    label: "Logout",
+                    onClick: onLogout,
+                    className:
+                      "!text-red-600 hover:!bg-red-100 [&.ant-menu-item-selected]:!bg-red-600",
+                  },
+                  {
+                    key: "settings",
+                    icon: <Gear size={16} weight="duotone" />,
+                    label: "Settings",
+                    onClick: () => navigate("/settings"),
+                  },
+                  {
+                    key: "help",
+                    icon: <Headset size={16} weight="duotone" />,
+                    label: "Help & support",
+                    onClick: () => navigate("/help-and-support"),
+                  },
+                  {
+                    key: "community",
+                    icon: <Smiley size={16} weight="duotone" />,
+                    label: "Community",
+                    onClick: () => navigate("/community"),
+                  },
+                ]}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="grid grid-cols-[auto_1fr_auto] px-1.5 py-1.5 items-center gap-1.5 overflow-hidden mt-auto w-full bg-gray-100 rounded-lg border border-gray-200">
         <Avatar
           className="flex-grow-0 flex-shrink-0 bg-gray-500 text-white"
           src="ellipse-12.png"
@@ -127,7 +179,7 @@ const Sidebar = () => {
         </div>
 
         <CaretDown weight="duotone" size={16} className="text-gray-500" />
-      </div>
+      </div> */}
     </aside>
   );
 };
