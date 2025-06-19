8d2a9c75f6f5855f0ed65897120fda464c88515e
Girish Sawant <girishsawant999.gs@gmail.com>
2025-06-19 16:34:54 +0530

    feat: implement toggle functionality for sidebar menu

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -17,6 +17,7 @@ import {
 } from "@phosphor-icons/react";
 import { Button, Divider, Menu } from "antd";
 import clsx from "clsx";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PlaceOrderWrapper from "../PlaceOrderWrapper";
 
@@ -80,6 +81,7 @@ const MENU_ITEMS = [
 const Sidebar = () => {
   const navigate = useNavigate();
   const { onLogout, user } = useAuthContext();
+  const [open, setOpen] = useState(false);
 
   return (
     <aside className="w-[220px] bg-white px-4 py-5 overflow-y-auto flex flex-col h-[calc(100vh-70px)]">
@@ -113,9 +115,10 @@ const Sidebar = () => {
         <div className={clsx("w-full")}>
           <div
             role="button"
+            onClick={() => setOpen((prev) => !prev)}
             className={clsx(
               "group grid grid-rows-[1fr] items-center rounded-lg bg-white py-1.5 shadow-[0px_0px_0px_1px_#dadadd] transition-[grid-template-rows,max-height,opacity] duration-300 ease-in-2 hover:bg-gray-50",
-              "group-hover:grid-rows-[1fr_auto]"
+              open && "grid-rows-[1fr_auto]"
             )}
           >
             <div className="flex items-center px-2">
@@ -137,14 +140,17 @@ const Sidebar = () => {
                 <CaretDown
                   weight="fill"
                   size={16}
-                  className="text-gray-500 group-hover:rotate-180 transition-transform duration-300 ease-in-2"
+                  className={clsx(
+                    "text-gray-500  transition-transform duration-300 ease-in-2",
+                    open && "rotate-180"
+                  )}
                 />
               </div>
             </div>
             <div
               className={clsx(
                 "grid max-h-0 gap-2 overflow-hidden opacity-0 transition-[max-height,opacity] duration-300 ease-in-2",
-                "group-hover:max-h-40 opacity-100"
+                open && "max-h-40 opacity-100"
               )}
             >
               <hr className="mx-2 mt-2 border-gray-200" />
@@ -189,25 +195,6 @@ const Sidebar = () => {
           </div>
         </div>
       </div>
-
-      {/* <div className="grid grid-cols-[auto_1fr_auto] px-1.5 py-1.5 items-center gap-1.5 overflow-hidden mt-auto w-full bg-gray-100 rounded-lg border border-gray-200">
-        <Avatar
-          className="flex-grow-0 flex-shrink-0 bg-gray-500 text-white"
-          src="ellipse-12.png"
-          children={user?.first_name?.[0] ?? ""}
-        />
-
-        <div className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative overflow-hidden truncate">
-          <p className="flex-grow-0 leading-none  flex-shrink-0 text-sm font-semibold text-left text-[#0a1b39] truncate">
-            {user?.first_name ?? ""} {user?.last_name ?? ""}
-          </p>
-          <p className="flex-grow-0 leading-none  flex-shrink-0 text-xs text-left text-[#83899f] truncate ">
-            {user?.role}
-          </p>
-        </div>
-
-        <CaretDown weight="duotone" size={16} className="text-gray-500" />
-      </div> */}
     </aside>
   );
 };
